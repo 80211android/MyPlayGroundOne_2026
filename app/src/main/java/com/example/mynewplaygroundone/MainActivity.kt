beeb6c594f73901fe2b50575e32f5613d838085f
droidmac <dorid@thedroid.com>
2026-06-10 18:43:41 -0400

    Adds MyScreen Composable

--- app/src/main/java/com/example/mynewplaygroundone/MainActivity.kt
+++ app/src/main/java/com/example/mynewplaygroundone/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mynewplaygroundone.ui.screens.myScreen
+import com.example.mynewplaygroundone.ui.screens.MyScreen
 import com.example.mynewplaygroundone.ui.theme.MyNewPlaygroundOneTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,35 +14,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyNewPlaygroundOneTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-
-                    myScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyNewPlaygroundOneTheme {
 //        Greeting("Android")
-        myScreen()
     }
 }
--- app/src/main/java/com/example/mynewplaygroundone/ui/screens/MyFirstScreen.kt
+++ app/src/main/java/com/example/mynewplaygroundone/ui/screens/MyFirstScreen.kt
@@ -1,21 +1,52 @@
 package com.example.mynewplaygroundone.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.mynewplaygroundone.components.SampleAppBar
+import com.example.mynewplaygroundone.ui.theme.MyNewPlaygroundOneTheme
+
 
 @Composable
-fun myScreen(modifier: Modifier = Modifier) {
+fun MyScreen() {
 
-    LazyColumn {
+    MyNewPlaygroundOneTheme {
+        Scaffold(
+            topBar = { SampleAppBar(
+//                        navController = navController,
+                elevation = 7.dp
+            ) },
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            PostDisplay(
+                modifier = Modifier
+//                            .padding(innerPadding)
+                    .padding(top = innerPadding.calculateTopPadding())
+                    .fillMaxSize()
+
+            )
+        }
+    }
+}
+
+
+@Composable
+fun PostDisplay(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier
+    ) {
         // Add a single item
         item {
             Text(text = "First item")
         }
 
         // Add 5 items
-        items(5) { index ->
+        items(8) { index ->
             Text(text = "Item: $index")
         }
 
@@ -24,4 +55,5 @@ fun myScreen(modifier: Modifier = Modifier) {
             Text(text = "Last item")
         }
     }
+
 }
