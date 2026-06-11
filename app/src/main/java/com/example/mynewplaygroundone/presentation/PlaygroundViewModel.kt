4bd4cd9caf2e14d92efda5c49df4158760a06d2d
droidmac <dorid@thedroid.com>
2026-06-10 20:20:15 -0400

    Adds hilt to viewModel

--- app/src/main/java/com/example/mynewplaygroundone/presentation/PlaygroundViewModel.kt
+++ app/src/main/java/com/example/mynewplaygroundone/presentation/PlaygroundViewModel.kt
@@ -3,11 +3,15 @@ package com.example.mynewplaygroundone.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class PlaygroundViewModel: ViewModel() {
+
+@HiltViewModel
+class PlaygroundViewModel @Inject constructor() : ViewModel() {
 
     private val _name: MutableLiveData<String> = MutableLiveData("")
 
