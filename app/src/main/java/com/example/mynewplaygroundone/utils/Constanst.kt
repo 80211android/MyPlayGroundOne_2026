ffd572b3c52711ad6dcbd0b4a54d6a5cc5ef42a1
droidmac <dorid@thedroid.com>
2026-06-11 17:24:07 -0400

    Adds constants file

---
+++ app/src/main/java/com/example/mynewplaygroundone/utils/Constanst.kt
@@ -0,0 +1,8 @@
+package com.example.mynewplaygroundone.utils
+
+import androidx.compose.ui.unit.dp
+
+const val BASE_URL = "https://intel.com/"
+
+val AppBarCollapsedHeight = 56.dp
+val AppBarExpendedHeight = 400.dp
