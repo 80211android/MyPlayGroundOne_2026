502c8cb6485eac5fb6b488d6617c39ad54ce4eef
droidmac <dorid@thedroid.com>
2026-06-11 19:52:32 -0400

    Adds new routes

--- app/src/main/java/com/example/mynewplaygroundone/navigation/PlaygroundNavigation.kt
+++ app/src/main/java/com/example/mynewplaygroundone/navigation/PlaygroundNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.mynewplaygroundone.navigation
 
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -7,15 +8,23 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mynewplaygroundone.data.FakePostsRepository
+import com.example.mynewplaygroundone.data.recipe.strawberryCake
 import com.example.mynewplaygroundone.presentation.PlaygroundViewModel
 import com.example.mynewplaygroundone.presentation.ui.screens.MyScreen
+import com.example.mynewplaygroundone.presentation.ui.screens.myscaffold.ScaffoldExample
+import com.example.mynewplaygroundone.presentation.ui.screens.recipe.MainFragment
+import com.example.mynewplaygroundone.presentation.ui.theme.White
 
 @Composable
 fun PlaygroundNavigation(){
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = "posts"
+//        startDestination = "posts",
+//        startDestination = "myrecipe",
+        startDestination = "myscaffold",
+
+
     ) {
 
         composable("posts") {
@@ -29,6 +38,23 @@ fun PlaygroundNavigation(){
             )
 
             MyScreen(navController = navController, playgroundViewModel = playgroundViewModel)
+        }
+
+        composable("myrecipe") {
+            Surface(color = White) {
+                MainFragment(strawberryCake)
+            }
+        }
+
+        composable("myscaffold") {
+            val onClick = {
+                navController.navigate("posts") {
+                    popUpTo(navController.graph.id) {
+                        saveState = true
+                    }
+                }
+            }
+            ScaffoldExample(onClick)
         }
 
     }
