22264330ff4850b3defc83d35f130223efd75db3
droidmac <dorid@thedroid.com>
2026-06-10 20:19:44 -0400

    Adds SampleAndroidApplication.kt

---
+++ app/src/main/java/com/example/mynewplaygroundone/SampleAndroidApplication.kt
@@ -0,0 +1,7 @@
+package com.example.mynewplaygroundone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SampleAndroidApplication : Application()
