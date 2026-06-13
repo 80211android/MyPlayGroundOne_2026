f73db063cbb336062e987b3e0597ac6766d4916a
droidmac <dorid@thedroid.com>
2026-06-13 13:58:26 -0400

    Adds Inject annotation to FakeRepository

--- app/src/main/java/com/example/mynewplaygroundone/data/FakePostsRepository.kt
+++ app/src/main/java/com/example/mynewplaygroundone/data/FakePostsRepository.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.mynewplaygroundone.data
 
 import com.example.mynewplaygroundone.data.posts.posts
@@ -27,12 +11,13 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 /**
  * Implementation of PostsRepository that returns a hardcoded list of
  * posts with resources after some delay in a background thread.
  */
-class FakePostsRepository : PostsRepository {
+class FakePostsRepository @Inject constructor() : PostsRepository {
 
     // for now, store these in memory
     private val favorites = MutableStateFlow<Set<String>>(setOf())
