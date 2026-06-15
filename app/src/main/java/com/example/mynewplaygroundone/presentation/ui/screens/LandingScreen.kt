f3723208d1647806b128771044fce392c51e5ad8
droidmac <dorid@thedroid.com>
2026-06-15 17:55:22 -0400

    Adds LandingScreen.kt composable

---
+++ app/src/main/java/com/example/mynewplaygroundone/presentation/ui/screens/LandingScreen.kt
@@ -0,0 +1,32 @@
+package com.example.mynewplaygroundone.presentation.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.mynewplaygroundone.R
+import kotlinx.coroutines.delay
+
+private const val SplashWaitTime: Long = 2000
+
+@Composable
+fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        // Adds composition consistency. Use the value when LaunchedEffect is first called
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+//        val currentOnTimeout by remember { mutableStateOf(onTimeout) }
+
+
+        LaunchedEffect(Unit) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
+        Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
+    }
+}
