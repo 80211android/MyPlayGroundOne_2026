610123325a4fe20c83b8e70a06d13892f5cb1cc9
droidmac <dorid@thedroid.com>
2026-06-15 17:56:53 -0400

    Launches Splash screen

--- app/src/main/java/com/example/mynewplaygroundone/MainActivity.kt
+++ app/src/main/java/com/example/mynewplaygroundone/MainActivity.kt
@@ -5,10 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mynewplaygroundone.navigation.PlaygroundNavigation
 import com.example.mynewplaygroundone.presentation.ui.theme.MyNewPlaygroundOneTheme
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.mynewplaygroundone.presentation.ui.screens.LandingScreen
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -16,6 +24,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            MyNewPlaygroundOneTheme{
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+private fun MainScreen() {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        var showLandingScreen by remember { mutableStateOf(true) }
+
+        if (showLandingScreen) {
+            LandingScreen(onTimeout = { showLandingScreen = false })
+        } else {
             PlaygroundNavigation()
         }
     }
