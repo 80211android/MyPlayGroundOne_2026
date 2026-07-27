49c17bc9d65e9a2023d754e24f5ba26c7ad2a401
droidmac <dorid@thedroid.com>
2026-07-27 00:40:51 -0400

    Makes Icon rotate

--- app/src/main/java/com/example/mynewplaygroundone/presentation/ui/screens/LandingScreen.kt
+++ app/src/main/java/com/example/mynewplaygroundone/presentation/ui/screens/LandingScreen.kt
@@ -1,19 +1,50 @@
 package com.example.mynewplaygroundone.presentation.ui.screens
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import com.example.mynewplaygroundone.R
-import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
+
+@Composable
+fun RotatingIcon() {
+    val infiniteTransition = rememberInfiniteTransition(label = "rotation")
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "rotation"
+    )
+
+    Image(
+        painterResource(id = R.drawable.ic_crane_drawer),
+        contentDescription = null,
+        modifier = Modifier.
+        rotate(36F)
+            .graphicsLayer{
+                rotationY = rotation
+            }
+
+    )
+}
 
 @Composable
 fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
@@ -23,10 +54,10 @@ fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
 //        val currentOnTimeout by remember { mutableStateOf(onTimeout) }
 
 
-        LaunchedEffect(Unit) {
-            delay(SplashWaitTime)
-            currentOnTimeout()
-        }
-        Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
+//        LaunchedEffect(Unit) {
+//            delay(SplashWaitTime)
+//            currentOnTimeout()
+//        }
+        RotatingIcon()
     }
 }
